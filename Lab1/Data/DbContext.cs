6c14244c1452a56f1a7006d7a111d98e111e2581
Vadym Tsyhanash <vadym.tsyhanash@gmail.com>
2022-09-24 22:16:38 +0300

    Lab1 Commit

---
+++ Lab1/Data/DbContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace Lab1.Data;
+
+internal class DbContext : IDisposable
+{
+	private readonly SqlConnection _conn;
+
+	public SqlConnection Connection
+	{
+		get
+		{
+			if (_conn.State != ConnectionState.Open)
+			{
+				_conn.Open();
+			}
+
+			return _conn;
+		}
+	}
+
+	public DbContext(string connectionString)
+	{
+		_conn = new SqlConnection(connectionString: connectionString);
+	}
+
+	public void Dispose()
+	{
+		_conn.Dispose();
+	}
+}
