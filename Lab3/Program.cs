d230fac274788c70589987d55645b30c7ef187c1
Vadym Tsyhanash <vadym.tsyhanash@gmail.com>
2022-10-12 09:02:44 +0300

    Lab3 Commit

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,8 +1,7 @@
 using Lab3.Models;
-using Lab3.Pages;
 using Lab3.Repositories;
-using Microsoft.AspNetCore.Mvc;
 using MongoDB.Driver;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,16 +12,37 @@ builder.Services.AddSingleton(s =>
     return mongoClient.GetDatabase(builder.Configuration.GetSection("LibraryDatabase")["Name"]);
 });
 builder.Services.AddSingleton(typeof(IRepository<>), typeof(GenericRepository<>));
-builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-app.MapGet("/", () =>
+app.MapGet("/", async (HttpContext context) =>
 {
-    return Results.Redirect("/Index");
-});
+    var bookBorrowRepository = app.Services.GetRequiredService<IRepository<BookBorrow>>();
+    var bookBorrows = (await bookBorrowRepository.GetAllAsync()).GroupBy(b => new { b.Reader.FirstName, b.Reader.LastName });
 
-app.MapRazorPages();
+    context.Response.Headers.ContentType = new Microsoft.Extensions.Primitives.StringValues("text/html; charset=UTF-8");
+
+    var bookBorrowsString = new StringBuilder();
+    bookBorrowsString.AppendLine("<img src=\"/img/logo.svg\" alt=\"logo\" width='100' height='100'/>");
+    foreach (var bookBorrow in bookBorrows)
+    {
+        bookBorrowsString.AppendLine("<div>");
+        bookBorrowsString.AppendLine($"<h2>{bookBorrow.Key.FirstName} {bookBorrow.Key.LastName}</h2>");
+        bookBorrowsString.AppendLine($"<p>Has borrowed {bookBorrow.Count()} book(s)</p>");
+        bookBorrowsString.AppendLine("<ul>");
+        foreach (var bookInfo in bookBorrow)
+        {
+            bookBorrowsString.AppendLine($"<li>{bookInfo.Book.Name}</li>");
+        }
+        bookBorrowsString.AppendLine("</ul>");
+        bookBorrowsString.AppendLine("</div>");
+    }
+
+    await context.Response.WriteAsync(text: "<html><body>" +
+        "<h1>Library</h1>" +
+        bookBorrowsString.ToString() +
+        "<body></html>");
+});
 
 app.UseStaticFiles();
 
