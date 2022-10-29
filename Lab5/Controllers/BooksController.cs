207caccc1f9806abbf6040d95b645a32464a28f2
Vadym Tsyhanash <vadym.tsyhanash@gmail.com>
2022-10-29 22:06:30 +0300

    Lab5 Commit

--- Lab5/Controllers/BooksController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Lab5.Models;
-using Lab5.Repositories;
-
-namespace Lab5.Controllers;
-
-public sealed class BooksController : GenericController<Book>
-{
-    public BooksController(IRepository<Book> repository) : base(repository) { }
-}
