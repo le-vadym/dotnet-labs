1bf4787eaec5970a7cb1ca8c6fa42c2e3d051e92
Vadym Tsyhanash <vadym.tsyhanash@gmail.com>
2022-10-30 17:18:18 +0200

    Lab6 Commit

---
+++ Lab6/Program.cs
@@ -0,0 +1,38 @@
+using Lab6.Data;
+using Lab6.Models;
+using Lab6.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<LibraryDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddScoped(typeof(IRepository<>), typeof(GenericRepository<>));
+builder.Services.AddScoped<IRepository<Reader>, ReadersRepository>();
+builder.Services.AddScoped<IRepository<BookBorrow>, BookBorrowRepository>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
---
+++ Lab6/Repositories/GenericRepository.cs
@@ -0,0 +1,59 @@
+﻿using Lab6.Data;
+using Lab6.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab6.Repositories;
+
+public class GenericRepository<T> : IRepository<T> where T : ModelBase
+{
+    protected readonly LibraryDbContext Context;
+
+    public GenericRepository(LibraryDbContext context) => Context = context;
+
+    public virtual async Task<T> CreateAsync(T entity)
+    {
+        var result = await Context.Set<T>().AddAsync(entity);
+        await Context.SaveChangesAsync();
+
+        return result.Entity;
+    }
+
+    public virtual async Task<bool> DeleteAsync(Guid id)
+    {
+        var entity = await Context.Set<T>().FirstOrDefaultAsync(e => e.Id == id);
+        if (entity == null)
+        {
+            return false;
+        }
+        Context.Set<T>().Remove(entity!);
+        await Context.SaveChangesAsync();
+
+        return true;
+    }
+
+    public virtual async Task<bool> ExistsAsync(Guid id)
+    {
+        return await Context.Set<T>().AnyAsync(e => e.Id == id);
+    }
+
+    public virtual async Task<IEnumerable<T>> GetAllAsync()
+    {
+        return await Context.Set<T>().ToListAsync();
+    }
+
+    public virtual async Task<T> GetAsync(Guid id)
+    {
+        var entity = await Context.Set<T>().FirstOrDefaultAsync(e => e.Id == id);
+
+        return entity!;
+    }
+
+    public virtual async Task<T> UpdateAsync(Guid id, T entity)
+    {
+        entity.Id = id;
+        var result = Context.Set<T>().Update(entity);
+        await Context.SaveChangesAsync();
+
+        return result.Entity;
+    }
+}
